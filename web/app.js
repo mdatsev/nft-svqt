ac1efbdee8d56cc82b9d6e822ac653aa5dbe1a67
toniuyt123 <antoniomilev123@gmail.com>
2022-01-31 16:55:36 +0200

    panning and path

--- web/app.js
+++ web/app.js
@@ -5,8 +5,8 @@ const app = express();
 const port = 3000;
 
 app.set('view engine', 'hbs');
-app.set('views', './views');
-app.use(express.static('public'));
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.render('index', {});
--- web/public/scripts/visualization.js
+++ web/public/scripts/visualization.js
@@ -1,14 +1,18 @@
 $(async () => {
     const canvas = document.getElementById("world");
     const ctx = canvas.getContext('2d');
-    const worldSize = 5;
+    const worldSize = 15;
     const imgSize = 100;
     // const tiles = GetWorld();
 
     ctx.canvas.width  = window.innerWidth - 200;
     ctx.canvas.height = window.innerHeight - 50;
 
+    const boundaryX = worldSize*imgSize - ctx.canvas.width;
+    const boundaryY = worldSize*imgSize - ctx.canvas.height;
     let lastX, lastY, dragStart;
+    let deltaX = 0;
+    let deltaY = 0;
 
     const img = await getImage('images/tile.jpg');
 
@@ -38,22 +42,47 @@ $(async () => {
     canvas.addEventListener('mousedown',function(evt){
         lastX = evt.offsetX || (evt.pageX - canvas.offsetLeft);
         lastY = evt.offsetY || (evt.pageY - canvas.offsetTop);
-        dragStart = {x: lastX, y: lastY};
-        dragged = false;
+        dragStart = true;
     },false);
 
     canvas.addEventListener('mousemove',function(evt){
-        lastX = evt.offsetX || (evt.pageX - canvas.offsetLeft);
-        lastY = evt.offsetY || (evt.pageY - canvas.offsetTop);
+        currentX = evt.offsetX || (evt.pageX - canvas.offsetLeft);
+        currentY = evt.offsetY || (evt.pageY - canvas.offsetTop);
 
         if (dragStart) {
-            ctx.translate(lastX-dragStart.x,lastY-dragStart.y);
+            let dx = currentX - lastX;
+            let dy = currentY - lastY;
+
+            if (deltaX + dx > 0 || deltaX + dx < -boundaryX) {
+                dx = 0;
+            }
+            if (deltaY + dy > 0 || deltaY + dy < -boundaryY) {
+                dy = 0;
+            }
+
+            ctx.translate(dx, dy);
+            deltaX += dx;
+            deltaY += dy;
             redraw();
         }
+
+        lastX = currentX;
+        lastY = currentY;
     },false);
 
     canvas.addEventListener('mouseup',function(evt){
-        dragStart = null;
+        dragStart = false;
+    },false);
+    canvas.addEventListener('mouseleave',function(evt){
+        dragStart = false;
+    },false);
+
+    canvas.addEventListener('mousewheel',function(evt){
+        console.log(evt);
+        const factor = evt.wheelDelta/100;
+
+        ctx.scale(factor,factor);
+        redraw();
     },false);
 
     redraw();
